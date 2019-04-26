aa74c122774c34e95ab48de078be0668cb075898
MKGOLD <dkqk0124@naver.com>
2019-04-26 19:27:56 +0900

    [Branch] Make Basic MVVM - StartActivity with Interface

--- app/src/main/java/com/example/mvvmsample/MainActivity.kt
+++ app/src/main/java/com/example/mvvmsample/MainActivity.kt
@@ -1,24 +1,23 @@
 package com.example.mvvmsample
 
+import android.content.Intent
 import android.databinding.DataBindingUtil
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import com.example.mvvmsample.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), CallAnotherActivityInterface{
     private lateinit var binding : ActivityMainBinding
-    private lateinit var adapter: MainViewPagerAdapter
     private lateinit var vm : MainViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
-        adapter = MainViewPagerAdapter(supportFragmentManager)
-        vm = MainViewModel()
-
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        vm = MainViewModel(this)
         binding.vm = vm
-        binding.viewPager.adapter = adapter
-        vm.maxPage.set(adapter.count)
-        vm.onCreate()
+    }
+
+    override fun callActivity() {
+        startActivity(Intent(this@MainActivity, SubActivity::class.java))
     }
 }
--- app/src/main/java/com/example/mvvmsample/MainViewModel.kt
+++ app/src/main/java/com/example/mvvmsample/MainViewModel.kt
@@ -1,31 +1,7 @@
 package com.example.mvvmsample
 
-import android.arch.lifecycle.ViewModel
-import android.databinding.Observable
-import android.databinding.ObservableBoolean
-import android.databinding.ObservableInt
-import android.view.View
-
-class MainViewModel : ViewModel() {
-    val currentPage = ObservableInt(0)
-    val maxPage = ObservableInt()
-    val isFirstPage = ObservableBoolean()
-    val isLastPage = ObservableBoolean()
-
-    fun onCreate() {
-        currentPage.addOnPropertyChangedCallback(object : Observable.OnPropertyChangedCallback() {
-            override fun onPropertyChanged(sender: Observable?, propertyId: Int) {
-                pageMoved()
-            }
-        })
-        isFirstPage.set(true)
+class MainViewModel(private val navigator : CallAnotherActivityInterface) {
+    fun callActivity() {
+        navigator.callActivity()
     }
-
-    private fun pageMoved() {
-        isFirstPage.set(currentPage.get() == 0)
-        isLastPage.set(currentPage.get() == maxPage.get() - 1)
-    }
-
-    val pageChangeButtonListener =
-        View.OnClickListener { view -> currentPage.set(currentPage.get() + Integer.valueOf(view?.tag.toString())) }
 }
